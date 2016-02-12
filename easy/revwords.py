93efe4aec1a04802327bf2761117e14ca59555c2
pankrato <ivan-p@yandex.ru>
2016-02-12 13:20:32 +0200

    improved version

--- easy/revwords.py
+++ easy/revwords.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 
-#https://www.codeeval.com/open_challenges/20/
+#https://www.codeeval.com/open_challenges/8/
 
 import sys
-
-separator = " "
 
 f = open(sys.argv[1], 'r')
 
 for string in f:
-	words = string.split( )
-	revers = separator.join(words[::-1])
-	print revers
-
+	words = string.split()
+	print " ".join(words[::-1])
 f.close()
 
